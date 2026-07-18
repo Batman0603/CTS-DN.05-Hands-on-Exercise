f7d94667312d57d3f8c7806aec0c083e7c2328b2
Batman0603 <karthikeyan060311@gmail.com>
2026-07-18 16:56:46 +0530

    Update Commit - Completed Angular Hands on  task-1 in Exercise -2

--- Week 5/Angular_HandsOn/student-course-portal/src/app/pages/home/home.ts
+++ Week 5/Angular_HandsOn/student-course-portal/src/app/pages/home/home.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
-  styleUrl: './home.css',
+  styleUrl: './home.css'
 })
 export class Home {
+
+  // String Interpolation
+  portalName = 'Student Course Portal';
+
+  // Property Binding
+  isPortalActive = false;
+
+  // Event Binding
+  message = '';
+
+  /*
+[property]
+One-way binding from Component -> DOM.
+
+[(ngModel)]
+Two-way binding between Component <-> DOM.
+Changes in the component update the view,
+and user input updates the component automatically.
+*/
+
+  // Two-Way Binding
+  searchTerm = '';
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
 
 }
