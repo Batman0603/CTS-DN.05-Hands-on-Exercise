e85502529417f1deef128741f0542e45669abc15
Batman0603 <karthikeyan060311@gmail.com>
2026-07-18 23:11:32 +0530

    Update Commit  - Completed Angular hands on  exercise-4

--- Week 5/Angular_HandsOn/student-course-portal/src/app/pages/enrollment-form/enrollment-form.ts
+++ Week 5/Angular_HandsOn/student-course-portal/src/app/pages/enrollment-form/enrollment-form.ts
@@ -3,41 +3,33 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
-
-  selector:'app-enrollment-form',
-
-  imports:[
+  selector: 'app-enrollment-form',
+  standalone: true,
+  imports: [
     CommonModule,
     FormsModule
   ],
-
-  templateUrl:'./enrollment-form.html',
-
-  styleUrl:'./enrollment-form.css'
-
+  templateUrl: './enrollment-form.html',
+  styleUrl: './enrollment-form.css'
 })
-
 export class EnrollmentForm {
 
-  studentName='';
+  studentName = '';
+  studentEmail = '';
+  courseId: number | null = null;
+  preferredSemester = '';
+  agreeToTerms = false;
 
-  studentEmail='';
+  submitted = false;
 
-  courseId:number|null=null;
-
-  preferredSemester='';
-
-  agreeToTerms=false;
-
-  onSubmit(form:NgForm){
-
-    console.log("Form Value");
+  onSubmit(form: NgForm) {
 
     console.log(form.value);
-
-    console.log("Form Valid");
-
     console.log(form.valid);
+
+    if (form.valid) {
+      this.submitted = true;
+    }
 
   }
 
