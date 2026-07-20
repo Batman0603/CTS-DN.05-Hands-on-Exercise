e424892e035fe14e27e3acc01d924bca3957bf4e
Batman0603 <karthikeyan060311@gmail.com>
2026-07-20 06:41:12 +0530

    Update Commit - Completed task 1 Exercise-7 in angular hands on

--- Week 5/Angular_HandsOn/student-course-portal/src/app/pages/course-list/course-list.ts
+++ Week 5/Angular_HandsOn/student-course-portal/src/app/pages/course-list/course-list.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 import { CourseCard } from '../../components/course-card/course-card';
-
 import { CourseService } from '../../services/course.service';
-
 import { Course } from '../../models/course.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 
@@ -15,10 +13,15 @@ import { Course } from '../../models/course.model';
 
   standalone:true,
 
-  imports:[
+ imports:[
+
     CommonModule,
+
+    FormsModule,
+
     CourseCard
-  ],
+
+],
 
   templateUrl:'./course-list.html',
 
@@ -32,15 +35,23 @@ export class CourseList implements OnInit{
 
   courses:Course[]=[];
 
+  searchTerm='';
+
   constructor(
 
     private courseService:CourseService,
 
-    private router:Router
+    private router:Router,
+
+    private route:ActivatedRoute
 
   ){}
 
   ngOnInit():void{
+
+    this.searchTerm =
+
+      this.route.snapshot.queryParamMap.get('search') || '';
 
     setTimeout(()=>{
 
@@ -61,6 +72,26 @@ export class CourseList implements OnInit{
   openCourse(id:number){
 
       this.router.navigate(['courses',id]);
+
+  }
+
+  searchCourses(){
+
+      this.router.navigate(
+
+        ['courses'],
+
+        {
+
+          queryParams:{
+
+            search:this.searchTerm
+
+          }
+
+        }
+
+      );
 
   }
 
--- Week 5/Angular_HandsOn/student-course-portal/src/app/pages/courses-layout/courses-layout.ts
+++ Week 5/Angular_HandsOn/student-course-portal/src/app/pages/courses-layout/courses-layout.ts
@@ -3,20 +3,20 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
 
-  selector: 'app-courses-layout',
+  selector:'app-courses-layout',
 
-  standalone: true,
+  standalone:true,
 
-  imports: [
+  imports:[
     RouterOutlet
   ],
 
-  templateUrl: './courses-layout.html',
+  templateUrl:'./courses-layout.html',
 
-  styleUrl: './courses-layout.css'
+  styleUrl:'./courses-layout.css'
 
 })
 
-export class CoursesLayout {
+export class CoursesLayout{
 
 }
