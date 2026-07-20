edf836489ffaf53ede3b45df4465d35cd94987f4
Batman0603 <karthikeyan060311@gmail.com>
2026-07-20 06:28:58 +0530

    Update Commit - Completed  Part-1 of task 1  in angular hands on exercise-7

--- Week 5/Angular_HandsOn/student-course-portal/src/app/app.routes.ts
+++ Week 5/Angular_HandsOn/student-course-portal/src/app/app.routes.ts
@@ -1,36 +1,62 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './pages/home/home';
-import { CourseList } from './pages/course-list/course-list';
 import { StudentProfile } from './pages/student-profile/student-profile';
+
+import { CourseList } from './pages/course-list/course-list';
+import { CourseDetail } from './pages/course-detail/course-detail';
+import { CoursesLayout } from './pages/courses-layout/courses-layout';
+
 import { EnrollmentForm } from './pages/enrollment-form/enrollment-form';
 import { ReactiveEnrollmentForm } from './pages/reactive-enrollment-form/reactive-enrollment-form';
+
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
 
   {
-    path:'',
-    component:Home
+    path: '',
+    component: Home
   },
 
   {
-    path:'courses',
-    component:CourseList
+    path: 'courses',
+    component: CoursesLayout,
+
+    children: [
+
+      {
+        path: '',
+        component: CourseList
+      },
+
+      {
+        path: ':id',
+        component: CourseDetail
+      }
+
+    ]
+
   },
 
   {
-    path:'profile',
-    component:StudentProfile
+    path: 'profile',
+    component: StudentProfile
   },
 
   {
-    path:'enroll',
-    component:EnrollmentForm
+    path: 'enroll',
+    component: EnrollmentForm
   },
 
   {
-    path:'enroll-reactive',
-    component:ReactiveEnrollmentForm
+    path: 'enroll-reactive',
+    component: ReactiveEnrollmentForm
+  },
+
+  {
+    path: '**',
+    component: NotFound
   }
 
 ];
